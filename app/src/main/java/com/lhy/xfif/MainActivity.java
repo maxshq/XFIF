ee78c2f3ea88bf888ef13eec8cb74e2fa5aff8e4
maxshq <maxshqha@gmail.com>
2025-04-20 22:06:28 +0800

    Update MainActivity.java

--- app/src/main/java/com/lhy/xfif/MainActivity.java
+++ app/src/main/java/com/lhy/xfif/MainActivity.java
@@ -19,8 +19,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-// import com.tbruyelle.rxpermissions2.Permission;
-// import com.tbruyelle.rxpermissions2.RxPermissions;
 
 // import io.reactivex.functions.Consumer;
 
@@ -38,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
     private Config config;
     private ConfigPreferences configPreferences;
 
-    //RxPermissions
-    //private final RxPermissions rxPermissions = new RxPermissions(this);
     private final static String TAG = "MAIN_ACTIVITY";
 
     @Override
@@ -94,29 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * 申请读写内存卡权限
-     */
-    private void initPermissions() {
-        rxPermissions
-                .requestEach(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .subscribe(new Consumer<Permission>() {
-                    @Override
-                    public void accept(Permission permission) throws Exception {
-                        if (permission.granted) {
-                            // 用户已经同意该权限
-                            Log.d(TAG, permission.name + " is granted.");
-                        } else if (permission.shouldShowRequestPermissionRationale) {
-                            // 用户拒绝了该权限，没有选中『不再询问』（Never ask again）,那么下次再次启动时，还会提示请求权限的对话框
-                            Log.d(TAG, permission.name + " is denied. More info should be provided.");
-                        } else {
-                            // 用户拒绝了该权限，并且选中『不再询问』
-                            Log.d(TAG, permission.name + " is denied.");
-                        }
-                    }
-                });
 
-    }
 
     /**
      * 修改状态
